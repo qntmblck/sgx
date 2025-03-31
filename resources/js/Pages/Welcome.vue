1aaa2e0ee7ee3988727386077a501bb632b35d0c
qntmblck <jcomeaux@ug.uchile.cl>
2025-03-31 17:03:02 -0300

    nain

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -64,7 +64,7 @@
                 </div>
               </div>
               <div class="text-center">
-                <h1 class="text-balance text-5xl font-semibold tracking-tight text-white sm:text-7xl">Data to enrich your online business</h1>
+                <h1 class="text-balance text-5xl font-semibold tracking-tight text-white sm:text-7xl"> to enrich your online business</h1>
                 <p class="mt-8 text-pretty text-lg font-medium text-gray-400 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
                   <a href="#" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Get started</a>
