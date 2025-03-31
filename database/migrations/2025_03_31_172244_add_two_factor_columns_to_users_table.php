340aa62c7024a0b4f4c9825ba1af7bb4253b6504
qntmblck <jcomeaux@ug.uchile.cl>
2025-03-31 16:40:34 -0300

    main

--- database/migrations/2025_03_31_172244_add_two_factor_columns_to_users_table.php
+++ database/migrations/2025_03_31_172244_add_two_factor_columns_to_users_table.php
@@ -4,39 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_secret')) {
+                $table->text('two_factor_secret')->nullable();
+            }
 
-            $table->text('two_factor_recovery_codes')
-                ->after('two_factor_secret')
-                ->nullable();
-
-            $table->timestamp('two_factor_confirmed_at')
-                ->after('two_factor_recovery_codes')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+                $table->text('two_factor_recovery_codes')->nullable();
+            }
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-                'two_factor_confirmed_at',
-            ]);
+            if (Schema::hasColumn('users', 'two_factor_secret')) {
+                $table->dropColumn('two_factor_secret');
+            }
+
+            if (Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+                $table->dropColumn('two_factor_recovery_codes');
+            }
         });
     }
 };
